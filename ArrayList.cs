0f2fc04cb4553c3627615ec69360c0e73482d707
Andreas Gropp <andreas.gropp@outlook.com>
2017-12-02 15:59:03 +0100

    Removed unnecessary whitespaces and emtpy lines. Deleted unused variable. Created one file for one class.

---
+++ ArrayList.cs
@@ -0,0 +1,91 @@
+using System;
+
+namespace Linear
+{
+    class ArrayList
+    {
+        private int[] arr;
+        public int Count { get; set; }
+        public ArrayList()
+        {
+            this.arr = new int[4];
+        }
+        public void Insert(int index, int value)
+        {
+            int[] newArr = new int[arr.Length + 1];
+            
+            Array.Copy(this.arr, newArr, index);
+            newArr[index] = value;
+            Array.Copy(this.arr, index, newArr, index + 1, arr.Length - index);
+            this.arr = newArr;
+            Count++;
+        }
+        public void Clear()
+        {
+            this.arr = new int[4];
+            Count = 0;
+        }
+        public int Remove(int number)
+        {
+            int result = -1;
+            
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (number == this.arr[i])
+                {
+                    int[] newArr = new int[arr.Length - 1];
+                    
+                    Array.Copy(this.arr, newArr, i);
+                    Array.Copy(this.arr, i+1, newArr, i, newArr.Length - i);
+                    this.arr = newArr;
+                    Count--;
+                    result = i;
+                    
+                    return result;
+                }
+            }
+            return result;
+        }
+        public bool Contains(int number)
+        {
+            bool result = false;
+            
+            foreach (int item in this.arr)
+            {
+                if (item == number)
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+        
+        public void Add(int number)
+        {
+            if (Count == arr.Length)
+            {
+                int[] newArr = new int[arr.Length * 2];
+                Array.Copy(this.arr, newArr, Count);
+                this.arr = newArr;
+            }
+            this.arr[Count++] = number;
+        }
+        
+        public int this[int index]
+        {
+            get
+            {
+                return this.arr[index];
+            }
+        }
+        public void DisplayArray()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.Write(this.arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    } 
+}
--- Program.cs
+++ Program.cs
@@ -1,158 +1,43 @@
 ﻿using System;
 
-class ArrayList
+namespace Linear
 {
-  private int[] arr;
-  public int Count { get; set; }
-  public int test;
-  
-  public ArrayList()
+  class Program 
   {
-    this.arr = new int[4];
-  }
-  
-  public void Insert(int index, int value)
-  {
-    int[] newArr = new int[arr.Length + 1];
-    
-    Array.Copy(this.arr, newArr, index);
-    newArr[index] = value;
-    Array.Copy(this.arr, index, newArr, index + 1, arr.Length - index);
-    this.arr = newArr;
-    Count++;
-    
-  }
-  
-  public void Clear()
-  {
-    this.arr = new int[4];
-    Count = 0;
-    
-    
-    
-  }
-  
-  public int Remove(int number)
-  {
-    int result = -1;
-    
-    for (int i = 0; i < arr.Length; i++)
+    static void Main()
     {
-      if (number == this.arr[i])
-      {
-        
-        
-        int[] newArr = new int[arr.Length - 1];
-        
-        Array.Copy(this.arr, newArr, i);
-        Array.Copy(this.arr, i+1, newArr, i, newArr.Length - i);
-        this.arr = newArr;
-        Count--;
-        result = i;
-        
-        return result;
-      }
+      ArrayList arr = new ArrayList();
+            
+      arr.Add(1);
+      arr.Add(2);
+      arr.Add(3);
+      arr.Add(4);
+      arr.Add(5);
+      arr.Add(3);
+      arr.Add(4);
+      arr.Add(5);
+      arr.Add(3);
+      arr.Add(4);
+      arr.Add(5);
       
+      arr.DisplayArray();
+      
+      Console.WriteLine(arr.Contains(5));
+      Console.WriteLine(arr.Contains(42));
+      
+      Console.WriteLine(arr.Remove(5));
+      arr.DisplayArray();     
+      
+      Console.WriteLine();
+      
+      arr.Insert(2, 42);
+      arr.DisplayArray();
+        
+      Console.WriteLine();
+      
+      arr.Clear();
+      arr.DisplayArray();
     }
-    
-    return result;
-  }
-  
-  public bool Contains(int number)
-  {
-    bool result = false;
-    
-    foreach (int item in this.arr)
-    {
-      if (item == number)
-      {
-        result = true;
-        break;
-      }
-    }
-    
-    return result;
-  }
-  
-  public void Add(int number)
-  {
-    if (Count == arr.Length)
-    {
-      int[] newArr = new int[arr.Length * 2];
-      Array.Copy(this.arr, newArr, Count);
-      this.arr = newArr;
-    }
-    this.arr[Count++] = number;
-  }
-  
-  public int this[int index]
-  {
-    get
-    {
-      return this.arr[index];
-    }
-  }
-  
-  
-  
-  public void DisplayArray()
-  {
-    for (int i = 0; i < Count; i++)
-    {
-      Console.Write(this.arr[i] + " ");
-    }
-    Console.WriteLine();
-  }
-} 
-
-class Task
-{
-  static void Main()
-  {
-    ArrayList arr = new ArrayList();
-    
-    
-    arr.Add(1);
-    arr.Add(2);
-    arr.Add(3);
-    arr.Add(4);
-    arr.Add(5);
-    arr.Add(3);
-    arr.Add(4);
-    arr.Add(5);
-    arr.Add(3);
-    arr.Add(4);
-    arr.Add(5);
-    
-    arr.DisplayArray();
-    
-    Console.WriteLine();
-    Console.WriteLine(arr.Contains(5));
-    Console.WriteLine(arr.Contains(42));
-    
-    Console.WriteLine(arr.Remove(5));
-    //Console.WriteLine(arr.Remove(42));
-    
-    for (int i = 0; i < arr.Count; i++)
-    {
-      Console.Write(arr[i] + " ");
-    }
-    Console.WriteLine();
-    
-    arr.Insert(2, 42);
-    arr.DisplayArray();
-    
-    
-    
-     Console.WriteLine();
-    
-    arr.Clear();
-    arr.DisplayArray();
-    
-   
-    
-    
-  }
-  
+  }  
 }
 
