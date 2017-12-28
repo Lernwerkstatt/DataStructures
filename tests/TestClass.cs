d83f14d01e078408297f82927daccfb5006eba3c
Andreas Gropp <andreas.gropp@outlook.com>
2017-12-28 06:14:15 +0100

    change namespace, variable names fully written, modified method Clear, removed commented out code, add TestClass class

---
+++ tests/TestClass.cs
@@ -0,0 +1,22 @@
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Linear;
+
+namespace Test
+{
+    class TestClass
+    {
+        ArrayList<ArrayListTests> testCases = new ArrayList<ArrayListTests>();
+        private int id;
+        private object value;
+
+        public int Id { get; set; }
+        public object Value { get; set; }
+
+        public TestClass(int id, object value)
+        {
+            //???
+        }
+    }
+}
